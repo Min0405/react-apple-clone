3fee4b41bba90d6e88642b6941d021b4b3972b8d
Min0405 <104662514+Min0405@users.noreply.github.com>
2023-01-12 11:39:08 +0900

    Update spatial.tsx

--- src/spatial.tsx
+++ src/spatial.tsx
@@ -1,17 +1,32 @@
-import useFade from "./hooks/ useFade";
+import { useEffect, useState } from "react";
+import useFade from "./hooks/ useFadeIn";
 import useScroll from "./hooks/useScroll";
 import './spatial.css'
 
 function Spatial() {
-    const scroll = useScroll();
-    const scrollPosition = useFade();
+  const scroll = useScroll(220);
+  const scrollPosition = useFade();
+  // const [changeUrl, setChangeUrl]= useState(String);
+
+  //scroll 값이 210 이상이 되면 섹션 이름 바꾸기
+
+console.log(scroll)
+
+// useEffect (() => {
+//   if (scroll > 210) {
+//     setChangeUrl("battery")
+//   } else {
+//     setChangeUrl("spatial-audio")
+//   }
+// }, [scroll])
+
 
   return(
     <div className="Spatial">
       <div className="Spatial-sticky-container">
         <header className="spatial-sticky">
           <div className="spatial-slide-container">
-            <div className="spatial-slide1"><img className="img" src={`https://www.apple.com/105/media/us/airpods-3rd-generation/2021/3c0b27aa-a5fe-4365-a9ae-83c28d10fa21/anim/spatial-audio/large/${scroll.toString().padStart(4, '0')}.jpg`} alt="php" /></div>
+            <div className="spatial-slide1"><img className="img" src={`https://www.apple.com/105/media/us/airpods-3rd-generation/2021/3c0b27aa-a5fe-4365-a9ae-83c28d10fa21/anim/${changeUrl}/large/${scroll.toString().padStart(4, '0')}.jpg`} alt="php" /></div>
             <div className="spatial-slide2"><h1 className={scrollPosition > 80 ? "scroll-text" : "scrolled-text"}>온몸을 휘감는 3차원 오디오의 <br/> 개인 맞춤형 공간 음향, </h1></div>
             {/* className을 삼항 연산자로 원하는 수치만큼 스크롤이 되면 바꿔주게 만든다 */}
           </div>
@@ -19,7 +34,7 @@ function Spatial() {
       </div>
     </div>
   )
-
 }
 
 export default Spatial;
+
