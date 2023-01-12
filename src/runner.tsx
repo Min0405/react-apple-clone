560d1f9e5d66de8df2f85788f7cbfc76d36ce883
Min0405 <104662514+Min0405@users.noreply.github.com>
2023-01-12 11:42:46 +0900

    Update runner.tsx

--- src/runner.tsx
+++ src/runner.tsx
@@ -1,44 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unused-expressions */
-import { useEffect, useState } from "react"
+import useFadeIn from "./hooks/ useFadeIn";
+import useFadeOut from "./hooks/useFadeOut";
+import useScroll from "./hooks/useScroll";
 import './runner.css'
 
 function Runner() {
-  // const [count, setCount] = useState(1);
-  const [scrolled, setScrolled] = useState(1);
+  const scroll = useScroll(1.0);    
 
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const [scrollPositions, setScrollPositions] = useState(0); //scroll 수치
-
-
-  useEffect(() => {
-    window.addEventListener("scroll", updateScroll);
-  }, []);
-  const updateScroll = () => {
-    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
-  };
-
-  const updateScrolled = () => {
-    setScrollPositions(window.scrollY || document.documentElement.scrollTop); //scroll Top의 위치를 알아내는 구문
-  };
-  useEffect(() => {
-    window.addEventListener("scroll", updateScrolled); //scroll 수치 업데이트
-  }, []);
-
-
-
-  useEffect(() => {
-    window.addEventListener("scroll", scrollProgress);
-    return () => window.removeEventListener("scroll", scrollProgress);
-  }, [])
-
-  const scrollProgress = () => {
-
-     const scrollpx = document.documentElement.scrollTop;
-     const winHeightPx = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-
-     const scrollLen = Math.ceil(scrollpx / winHeightPx * 100 / 1.0);
-     setScrolled(scrollLen)
-   }
+  const scrollPosition = useFadeIn();
+  const scrollPositions = useFadeOut();  
 
   return(
     <div className="Runner">
@@ -46,7 +15,7 @@ function Runner() {
         <header className="runner-sticky">
           <div className="runner-slide-container">
             {/* <img src={`https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`} alt="pho"/> */}
-            <div className="runner-slide1"><img className="img" src={`https://www.apple.com/105/media/us/airpods-3rd-generation/2021/3c0b27aa-a5fe-4365-a9ae-83c28d10fa21/anim/runner/large/${scrolled.toString().padStart(4, '0')}.jpg`} alt="phot" /></div>
+            <div className="runner-slide1"><img className="img" src={`https://www.apple.com/105/media/us/airpods-3rd-generation/2021/3c0b27aa-a5fe-4365-a9ae-83c28d10fa21/anim/runner/large/${scroll.toString().padStart(4, '0')}.jpg`} alt="phot" /></div>
             <div className="runner-slide2"><h1 className={scrollPosition > 760 ? "scrolled-text1" : "scroll-text1"}>비가 와도, <br/> 땀이 비 오듯 해도 <br/> 끄떡없는 방수 기능까지.</h1></div>
             <div className="runner-slide3"><h1 className={scrollPositions > 810 ? "scroll-text2" : "scrolled-text2"}>사뭇 마법 같은 <br/> 경험을 선사합니다.</h1></div>
           </div>
