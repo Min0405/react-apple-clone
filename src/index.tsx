27d6059d61c3fc1bd91e8738330d4b97d9aefd22
Min0405 <104662514+Min0405@users.noreply.github.com>
2023-01-12 11:50:47 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -11,8 +10,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
         <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
